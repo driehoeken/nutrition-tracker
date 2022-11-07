f6cf6a7ea85afb5ac0f284c6757d49a1fbd0db96
driehoeken <a.bednarski1203@gmail.com>
2022-11-07 23:59:24 +0100

    dropdown

---
+++ src/components/menu/Dropdown.js
@@ -0,0 +1,7 @@
+import "./Dropdown.css";
+
+const Dropdown = (props) => {
+    return <div className="dropdown" id={`dropdown-${props.id}`}></div>;
+};
+
+export default Dropdown;
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,14 +1,42 @@
+import Dropdown from "../menu/Dropdown";
 import styles from "./Nav.module.css";
 
 const Nav = () => {
+    const navButtonHandler = (dropdownID) => {
+        const dropdownEl = document.getElementById(`dropdown-${dropdownID}`);
+        if (dropdownEl.classList.contains("hidden")) {
+            dropdownEl.classList.remove("hidden");
+            setTimeout(() => {
+                dropdownEl.classList.remove("visually-hidden");
+            }, 20);
+        } else {
+            dropdownEl.classList.add("visually-hidden");
+            dropdownEl.addEventListener(
+                "transitionend",
+                () => {
+                    dropdownEl.classList.add("hidden");
+                },
+                {
+                    capture: false,
+                    once: true,
+                    passive: false,
+                }
+            );
+        }
+    };
     return (
         <nav>
-            <div className={styles["nav-notification"] + " center"}>
+            <Dropdown id="notification" />
+            <button
+                className={styles["nav-notification"] + " center"}
+                onClick={() => navButtonHandler("notification")}
+            >
                 <i class="fa fa-bell" aria-hidden="true"></i>
-            </div>
-            <div className={styles["nav-user-button"] + " center"}>
+            </button>
+
+            <button className={styles["nav-user-button"] + " center"}>
                 <i class="fa fa-user" aria-hidden="true"></i>
-            </div>
+            </button>
             <p className={styles["nav-user-name"]}> Chris Buffet</p>
             <div className={styles["nav-user-menu"]}>
                 <i class="fa fa-caret-down" aria-hidden="true"></i>
